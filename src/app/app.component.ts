8693561b153d22f91c943efe5cda36742a1d271a
m.belogubova <m.belogubova@webvork.com>
2023-04-03 12:30:20 +0300

    init: angular cli and bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app.movies';
+  constructor(private modalService: NgbModal) {}
+
+  public openModal(modal: any) {
+    this.modalService.open(modal)
+  }
 }
